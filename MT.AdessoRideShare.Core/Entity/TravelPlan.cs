33cd65ad209354c35026248de654c923aaae65a2
Merve TOPAL <merve.tpl@windowslve.com>
2022-01-15 20:46:45 +0300

    *[development] *[core katmanı entity, interfaces oluşturulmuştur.]

---
+++ MT.AdessoRideShare.Core/Entity/TravelPlan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MT.AdessoRideShare.Core.Entity
+{
+   public class TravelPlan
+    {
+        public int id { get; set; }
+
+        public string WhereFrom  { get; set; }
+
+        public string ToWhere { get; set; }
+        public DateTime TravelTime { get; set; }
+
+        public int NumberOfSeats { get; set; }
+
+        public string Explanation { get; set; }
+
+    }
+}
+
---
+++ MT.AdessoRideShare.Core/Entity/User.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace MT.AdessoRideShare.Core.Entity
+{
+    public class User
+    {
+
+        public User()
+        {
+            ContactInfos = new Collection<TravelPlan>();
+        }
+        public int id { get; set; }
+        public string  Name { get; set; }
+
+        public string SurName { get; set; }
+
+        public ICollection<TravelPlan> ContactInfos { get; set; }
+    }
+}
