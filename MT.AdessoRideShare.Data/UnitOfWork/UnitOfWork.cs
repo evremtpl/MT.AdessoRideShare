3ff40e0ae2114a2b22d11f94c0f8c901ec7e49fa
Merve TOPAL <merve.tpl@windowslve.com>
2022-01-15 21:22:34 +0300

    *[development] *[*[Service katmanı için interface implementasyonları yapılmıştır.]

--- MT.AdessoRideShare.Data/UnitOfWork/UnitOfWork.cs
+++ MT.AdessoRideShare.Data/UnitOfWork/UnitOfWork.cs
@@ -1,9 +1,5 @@
 ﻿using MT.AdessoRideShare.Core.Interfaces.UnitOfWork;
 using MT.AdessoRideShare.Data.Context;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace MT.AdessoRideShare.Data.UnitOfWork
---
+++ MT.AdessoRideShare.Service/Services/Service.cs
@@ -0,0 +1,52 @@
+﻿using MT.AdessoRideShare.Core.Interfaces.Repository;
+using MT.AdessoRideShare.Core.Interfaces.Services;
+using MT.AdessoRideShare.Core.Interfaces.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace MT.AdessoRideShare.Service.Services
+{
+    public class Service<TEntity> : IService<TEntity> where TEntity : class, new()
+    {
+
+        public readonly IUnitOfWork _unitOfWork;
+
+        private readonly IGenericRepository<TEntity> _repository;
+        public Service(IUnitOfWork unitOfWork, IGenericRepository<TEntity> repository)
+        {
+            _unitOfWork = unitOfWork;
+            _repository = repository;
+        }
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+            await _repository.AddAsync(entity);
+            await _unitOfWork.CommitAsync();
+
+            return entity;
+        }
+
+
+        public void Delete(TEntity entity)
+        {
+            _repository.Delete(entity);
+            _unitOfWork.Commit();
+        }
+
+        public async Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _repository.Find(predicate);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _repository.GetAllAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync(int id)
+        {
+            return await _repository.GetByIdAsync(id);
+        }
+    }
+}
